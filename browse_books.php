d61df04cc75da3e25364185f9b064f15fa982e6c
audreylavillat <audrey.lavillat@efrei.net>
2017-10-11 14:35:03 +0200

    Search

--- browse_books.php
+++ browse_books.php
@@ -1,9 +1,33 @@
 <?php
     require_once 'assets/partial/header.php';
 
+if (isset($_POST['search_title']) && isset($_POST['search_author']))  {
+    if (!empty($_POST['search_title']) && !empty($_POST['search_author'])) {
+        $book = $bdd->prepare('SELECT b.*, a.*, b.id id_book FROM book b, author a, book_author ba WHERE b.id = ba.book_id AND a.id = ba.author_id AND reserved= :bool AND b.name LIKE :search_title AND (a.firstname LIKE :search_author OR a.lastname LIKE :search_author OR a.nationality LIKE :search_author)GROUP BY ba.id');
+        $book->execute(array(':bool' => 0, ':search_title' => '%' . $_POST['search_title'] . '%', ':search_author' => '%' . $_POST['search_author'] . '%'));
+    }
+    else {
+        if (!empty($_POST['search_title'])) {
+            $book = $bdd->prepare('SELECT b.*, a.*, b.id id_book FROM book b, author a, book_author ba WHERE b.id = ba.book_id AND a.id = ba.author_id AND reserved= :bool AND b.name LIKE :search_title GROUP BY ba.id');
+            $book->execute(array(':bool' => 0, ':search_title' => '%' . $_POST['search_title'] . '%'));
+        }
+        else{
+            if (!empty($_POST['search_author'])) {
+                $book = $bdd->prepare('SELECT b.*, a.*, b.id id_book FROM book b, author a, book_author ba WHERE b.id = ba.book_id AND a.id = ba.author_id AND reserved= :bool AND (firstname LIKE :search_author OR lastname LIKE :search_author OR nationality LIKE :search_author)GROUP BY ba.id');
+                $book->execute(array(':bool' => 0, ':search_author' => '%' . $_POST['search_author'] . '%'));
+            }
+            else{
+                $book = $bdd->prepare('SELECT b.*, a.*, b.id id_book FROM book b, author a, book_author ba WHERE b.id = ba.book_id AND a.id = ba.author_id AND reserved= :bool GROUP BY ba.id');
+                $book->execute(array(':bool' => 0));
+            }
+        }
+    }
 
-$book = $bdd->prepare('SELECT * FROM book WHERE reserved= :bool');
-$book->execute(array(':bool' => 0));
+}
+else{
+    $book = $bdd->prepare('SELECT b.*, a.*, b.id id_book FROM book b, author a, book_author ba WHERE b.id = ba.book_id AND a.id = ba.author_id AND reserved= :bool GROUP BY ba.id');
+    $book->execute(array(':bool' => 0));
+}
 
 generateHeader('browse_books');
 ?>
@@ -19,13 +43,13 @@ generateHeader('browse_books');
 <div class="center-class">
     <form action="browse_books.php" method="post">
         <div class="row">
-            <label for="title">Title :</label>
-            <input type="text" name="title" id="title" />
+            <label for="search_title">Title :</label>
+            <input type="text" name="search_title" id="search_title" />
         </div>
 
         <div class="row">
-            <label for="author">Author :</label>
-            <input type="text" name="author" id="author" />
+            <label for="search_author">Author :</label>
+            <input type="text" name="search_author" id="search_author" />
         </div>
 
         <div class="row">
@@ -55,7 +79,7 @@ generateHeader('browse_books');
             <th>Image</th>
             <th>Title</th>
             <th>Author</th>
-            <th></th>
+            <th>Reserve</th>
         </tr>
         </thead>
 
@@ -65,8 +89,8 @@ generateHeader('browse_books');
             ?>
             <td><img src="assets/img/<?php echo $donnees['img'];?>" alt="Picture of Alice" class="book-image" /></td>
             <td> <?php echo $donnees['name']; ?></td>
-            <td> <?php echo $donnees['author']; ?></td>
-            <td><a href="reserve.php<?php echo '?id='.$donnees['id'].''; ?>" class="button">Reserve</a></td>
+            <td> <?php echo $donnees['firstname']; ?> <?php echo $donnees['lastname']; ?>, (<?php echo $donnees['nationality']; ?>)</td>
+            <td><a href="reserve.php<?php echo '?id='.$donnees['id_book'].''; ?>" class="button">Reserve</a></td>
             </tr>
             <?php
         }
